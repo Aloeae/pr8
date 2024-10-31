924789c37d3e1011dd8df2cf7180b7eb27f6446e
Alyona Efremova <142985072+Aloeae@users.noreply.github.com>
2024-10-31 19:22:17 +0500

    Update hw2.py

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,7 +1,17 @@
 while True:
     try:
-        a = int(input("Введите первое целое число: "))
-        b = int(input("Введите второе целое число: "))
-        print("Сумма:", a + b)
+        a = input("Введите первое целое число или слово'stop' для выхода: ")
+        if a.lower() == "stop":
+            print("Выход из программы.")
+            break
+        num1 = int(a)
+        
+        b = input("Введите второе целое число или слово 'stop' для выхода: ")
+        if b.lower() == "stop":
+            print("Выход из программы.")
+            break
+        num2 = int(b)
+        
+        print("Сумма:", num1 + num2)
     except ValueError:
         print("Ошибка! Введите целое число.")
